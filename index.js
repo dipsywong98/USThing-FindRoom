9861f3ea13dc91ec93a80a892a07a0da26dd1cc5
dipsywong98 <facebookarmyoffcal@gmail.com>
2017-08-24 22:40:15 +0800

    build courses success

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ request('https://w5.ab.ust.hk/wcq/cgi-bin/1710/', function (error, response, bod
     //     GetCourseByURL(links[i]);
     // }
     
-     GetCourseByURL('https://w5.ab.ust.hk/wcq/cgi-bin/1710/');
+     PushCoursesByURL('https://w5.ab.ust.hk/wcq/cgi-bin/1710/');
     
 });
 
@@ -45,27 +45,25 @@ request('https://w5.ab.ust.hk/wcq/cgi-bin/1710/', function (error, response, bod
     }
  }
 
-function GetCourseByURL(url){
+function PushCoursesByURL(url){
     request(url ,function (error ,response ,body){  
 
         $ = cheerio.load(body);
 
-        var courses = $('div.course');
+        var raw_courses = $('div.course');
 
         //for each course in courses
-        var course = $(courses[0]); 
+        for(var i=0; i<raw_courses.length; i++){
+            var course = $(raw_courses[i]); 
+            all_courses.push(BuildCourse(course));
+        }
+        
+        console.log(all_courses);
+
         var title = GetInnerText(course.find('h2')[0]);
-        console.log(title);
+        // console.log(title);
         var sections = course.find('tr').filter('[class!=""]');
         BuildSections(sections);
-
-        //for each section in sections
-        var details = $(sections[0]).find('td');
-
-        var name = GetInnerText(details[0]);
-        var time = GetInnerText(details[1]);
-        // console.log(name);
-        // console.log(time);
     })
 }
 
@@ -75,9 +73,12 @@ function GetInnerText(element){
 
 function BuildSections(raw_sections){
     var sections = [];
+    //for each sections(tr)
     for(var i=0; i<raw_sections.length; i++){
+        //get its detials (td)
         var details = $(raw_sections[i]).find('td');
         if(raw_sections[i].attribs.class.indexOf('newsect') !== -1){
+            //new section 
             sections.push({
                 name:GetInnerText(details[0]),
                 class:[{
@@ -87,6 +88,7 @@ function BuildSections(raw_sections){
             })
         }
         else{
+            //if it is extened row for previous section
             sections[sections.length-1].class.push({
                 time:GetInnerText(details[0]),
                 location:GetInnerText(details[1])
@@ -94,4 +96,12 @@ function BuildSections(raw_sections){
         }
     }
     return sections;
+}
+
+function BuildCourse(raw_course){
+    var sections = raw_course.find('tr').filter('[class!=""]');
+    return {
+        title: GetInnerText(raw_course.find('h2')[0]),
+        sections: BuildSections(sections)
+    }
 }
