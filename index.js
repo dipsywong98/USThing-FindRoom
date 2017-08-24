821c4173bb94f2b66f003de32b5f3997465860ec
dipsywong98 <facebookarmyoffcal@gmail.com>
2017-08-25 00:19:26 +0800

    classes split two days

--- index.js
+++ index.js
@@ -16,27 +16,48 @@
  * ]
  */
 
+var request = require('request');
 const cheerio = require('cheerio');
 var fs = require('fs');
+var moment = require('moment');
+moment().format();
 
 var links = [];
 var all_courses = [];
 var load_sum = 0;
 var $;
 
+var days = ["Mo","Tu","We","Th","Fr","Sa","Su"];
 
-var request = require('request');
+
+function GetInnerText(element){
+    var text = "";
+    for(var i=0; i<element.children.length; i++){
+        if(element.children[i].data){
+            text+=element.children[i].data;
+        }
+        else{
+            text+="|";
+        }
+    }
+    return text;
+}
+
+function StrContain(str1, str2){
+    return str1.indexOf(str2) !== -1;
+}
+
 request('https://w5.ab.ust.hk/wcq/cgi-bin/1710/', function (error, response, body) {
     
     $ = cheerio.load(body);
 
-    GetCourseLinks();
+    // GetCourseLinks();
     // console.log(links);
-    for(var i=0; i<links.length; i++){
-        PushCoursesByURL(links[i]);
-    }
+    // for(var i=0; i<links.length; i++){
+    //     PushCoursesByURL(links[i]);
+    // }
     
-    //  PushCoursesByURL('https://w5.ab.ust.hk/wcq/cgi-bin/1710/');
+     PushCoursesByURL('https://w5.ab.ust.hk/wcq/cgi-bin/1710/');
     
 });
 
@@ -69,21 +90,16 @@ function PushCoursesByURL(url){
         }
 
         //loaded all courses
-        if(++load_sum==links.length){
+        if(++load_sum>=links.length){
             fs.writeFile("courses.json", JSON.stringify({courses:all_courses}), function(err) {
                 if(err) {
                     return console.log(err);
                 }
-
                 console.log("The file was saved!");
             }); 
             // console.log(all_courses);
         }
     })
-}
-
-function GetInnerText(element){
-    return element.children[0].data;
 }
 
 function BuildSections(raw_sections){
@@ -92,23 +108,65 @@ function BuildSections(raw_sections){
     for(var i=0; i<raw_sections.length; i++){
         //get its detials (td)
         var details = $(raw_sections[i]).find('td');
-        if(raw_sections[i].attribs.class.indexOf('newsect') !== -1){
+        if(StrContain(raw_sections[i].attribs.class,'newsect')){
             //new section 
             sections.push({
                 name:GetInnerText(details[0]),
-                class:[{
-                    time:GetInnerText(details[1]),
-                    location:GetInnerText(details[2])
-                }]
+                class:[]
             })
         }
         else{
             //if it is extened row for previous section
-            sections[sections.length-1].class.push({
-                time:GetInnerText(details[0]),
-                location:GetInnerText(details[1])
-            })
+            details.splice(0,0,"");
         }
+
+        BuildClasses(sections[sections.length-1],details);
     }
     return sections;
+}
+
+function BuildClasses(section,details){
+    var classes=[];
+    var time_str = GetInnerText(details[1]);
+    var location = GetInnerText(details[2]);
+    var note="N/A";
+    var start_time = 0;
+    var end_time = 0;
+
+    if(time_str=="TBA"&&location=="TBA"){
+        return "TBA";
+    }
+
+    if(StrContain(time_str,"|")){
+        var strs = time_str.split("|");
+        note = strs[0];
+        time_str = strs[1];
+    }
+
+    for(var i=0; i<days.length; i++){
+        time_str = time_str.replace(days[i]," "+days[i]);
+    }
+    time_strs = time_str.split(" ");
+    // console.log(time_strs);
+
+    for(var i=time_strs.length-1; i>0; --i){
+        var str = time_strs[i];
+        if(StrContain("MoTuWeThFrSaSu",str)){
+            //talking about week days
+            classes.push({
+                day:str,
+                start_time: start_time,
+                end_time:end_time,
+                location: location,
+                note: note
+            });
+        }
+        if(StrContain(str,":")){
+            console.log("hi");
+            if(end_time == 0) end_time = str;
+            else start_time = str;
+        }
+    }
+    console.log(classes);
+    return classes;
 }
